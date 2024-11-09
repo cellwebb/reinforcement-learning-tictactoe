6b3be795a13f4a937c269dc51405d6797cb7aa61
cellwebb <cellwebb@users.noreply.github.com>
2024-11-08 22:55:17 -0800

    Correct mistake in inverse mapping

--- src/tictactoe/constants.py
+++ src/tictactoe/constants.py
@@ -30,7 +30,7 @@ INVERSE_MAPPINGS = {
     "rotate_180": (8, 7, 6, 5, 4, 3, 2, 1, 0),
     "rotate_270": (6, 3, 0, 7, 4, 1, 8, 5, 2),
     "reflect_horizontal": (2, 1, 0, 5, 4, 3, 8, 7, 6),
-    "reflect_horizontal_rotate_90": (6, 3, 0, 7, 4, 1, 8, 5, 2),
-    "reflect_horizontal_rotate_180": (8, 7, 6, 5, 4, 3, 2, 1, 0),
+    "reflect_horizontal_rotate_90": (8, 5, 2, 7, 4, 1, 6, 3, 0),
+    "reflect_horizontal_rotate_180": (6, 7, 8, 3, 4, 5, 0, 1, 2),
     "reflect_horizontal_rotate_270": (0, 3, 6, 1, 4, 7, 2, 5, 8),
 }
